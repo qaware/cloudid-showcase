ffc48497255d379e4080624dc7ab97a8e4e792e5
Christian Fritz <christian.fritz@qaware.de>
2018-01-15 13:26:07 +0100

    Adds basic structure.

---
+++ src/main/java/de/qaware/cloud/id/demoserver/DemoServerApplication.java
@@ -0,0 +1,12 @@
+package de.qaware.cloud.id.demoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServerApplication.class, args);
+    }
+}
