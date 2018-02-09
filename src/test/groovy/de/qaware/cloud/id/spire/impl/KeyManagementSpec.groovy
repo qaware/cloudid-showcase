2b710982bfc6741196be05c46948d65c0f9cb8d6
Andreas Zitzelsberger <az@az82.de>
2018-02-09 11:55:50 +0100

    spock tests added

---
+++ src/test/groovy/de/qaware/cloud/id/spire/impl/KeyManagementSpec.groovy
@@ -0,0 +1,42 @@
+package de.qaware.cloud.id.spire.impl
+
+import spock.lang.Specification
+
+import javax.net.ssl.X509KeyManager
+import java.security.Principal
+
+/**
+ * Specification testing key management with SPIFFE.
+ */
+class KeyManagementSpec extends Specification {
+    SpiffeKeyManagerFactorySpi keyManagerFactory
+
+    void setup() {
+        keyManagerFactory = new SpiffeKeyManagerFactorySpi()
+        keyManagerFactory.engineInit(null)
+    }
+
+    def "get key managers"() {
+        when:
+        def keyManagers = keyManagerFactory.engineGetKeyManagers()
+
+        then:
+        keyManagers.length == 1
+        keyManagers[0] instanceof X509KeyManager
+    }
+
+    def "use key manager"() {
+        given:
+        def keyManager = (X509KeyManager) keyManagerFactory.engineGetKeyManagers()[0]
+
+        when:
+        // Wait for the updater thread to do it's work
+        Thread.sleep(2_000)
+
+        then:
+        keyManager.getPrivateKey("") != null
+        keyManager.getCertificateChain("").length > 0
+        keyManager.getClientAliases("", [] as Principal[]).toList() == ["spiffe://example.org/host/workload"]
+    }
+
+}
