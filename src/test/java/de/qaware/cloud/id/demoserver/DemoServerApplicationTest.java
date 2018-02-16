e68e8c9d75ab8c42347c5cd1a491afc2466df762
Andreas Zitzelsberger <az@az82.de>
2018-02-16 16:21:52 +0100

    test name fixed

--- src/test/java/de/qaware/cloud/id/demoserver/DemoServerApplicationTest.java
+++ src/test/java/de/qaware/cloud/id/demoserver/DemoServerApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoServerApplicationTests {
+public class DemoServerApplicationTest {
 
     @Test
     public void contextLoads() {
