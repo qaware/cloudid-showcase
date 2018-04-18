f834d4247623d33b3794cc16a44c786d00829a2e
Roman Buchholz <roman.buchholz@qaware.de>
2018-04-18 12:41:48 +0200

    Added Thymeleaf to generate the old demo page #33

--- cloudid/cloudid-demo/src/main/java/de/qaware/cloudid/demo/DemoResponder.java
+++ cloudid/cloudid-demo/src/main/java/de/qaware/cloudid/demo/DemoResponder.java
@@ -5,14 +5,14 @@ import de.qaware.cloudid.lib.IdManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+
+import static de.qaware.cloudid.demo.util.TraceFormatter.getFormattedTrace;
 
 /**
  * Simple proxy controller. Performs some requests against a configurable backend.
@@ -23,68 +23,22 @@ import java.util.Enumeration;
 @RequiredArgsConstructor
 public class DemoResponder {
 
+    private static final String RESPONSE_TEMPLATE_NAME = "demo";
     private final IdManager idManager = CloudId.getIdManager();
 
     /**
      * Generates a html demo page which contains the SPIFFE callstack, if available.
      *
-     * @param path    request path
      * @param request request
+     * @param model   model supplied by and for thymeleaf
+     * @param path    request path
      * @return response entity with the generated html page
      */
     @RequestMapping("/{path}")
-    public ResponseEntity demoPageRequest(@PathVariable String path, HttpServletRequest request) {
-        Enumeration<String> headers = request.getHeaders("X-SPIFFE-Callstack");
-        String trace;
-        if (headers.hasMoreElements()) {
-            trace = headers.nextElement();
-            LOGGER.debug("Received SPIFFE callstack: {}", trace);
-            trace = formatTrace(trace, true);
-        } else {
-            LOGGER.debug("Received empty SPIFFE callstack");
-            trace = "NO TRACE AVAILABLE!";
-        }
-        String responseBody = generateResponseBody(trace);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(responseBody);
-    }
-
-    private String generateResponseBody(String trace) {
-        String ourId = idManager.getSingleBundle().getSpiffeId();
-        String body = getBodyTemplate();
-        body = body.replace("%TRACE", trace);
-        body = body.replace("%MY_IDENTITY", ourId);
-        return body;
-    }
-
-    private String getBodyTemplate() {
-        return "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "<meta charset=\"UTF-8\">\n" +
-                "<title>CNI Demo</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h2>My identity:</h2>\n" +
-                "%MY_IDENTITY\n" +
-                "<h2>Call trace:</h2><br />\n" +
-                "%TRACE\n" +
-                "</body>\n" +
-                "</html>";
-    }
-
-    private String formatTrace(String trace, boolean appendOwnId) {
-        assert trace != null;
-        if (appendOwnId) {
-            trace += idManager.getSingleBundle().getSpiffeId();
-        }
-        String[] traces = trace.split("#");
-        StringBuilder traceBuilder = new StringBuilder();
-        traceBuilder.append("<ol>");
-        for (String currentTrace : traces) {
-            traceBuilder.append("<li>").append(currentTrace).append("</li><br />");
-        }
-        traceBuilder.append("</ol>");
-        return traceBuilder.toString();
+    public String demoPageRequest(HttpServletRequest request, Model model, @PathVariable("path") String path) {
+        model
+                .addAttribute("myid", idManager.getSingleBundle().getSpiffeId())
+                .addAttribute("trace", getFormattedTrace(request));
+        return RESPONSE_TEMPLATE_NAME;
     }
 }
---
+++ cloudid/cloudid-demo/src/main/java/de/qaware/cloudid/demo/util/TraceFormatter.java
@@ -0,0 +1,59 @@
+package de.qaware.cloudid.demo.util;
+
+import de.qaware.cloudid.lib.CloudId;
+import de.qaware.cloudid.lib.IdManager;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.splitByWholeSeparator;
+
+/**
+ * Utility class for retrieving and formatting a SPIFFE call trace
+ */
+@UtilityClass
+@Slf4j
+public class TraceFormatter {
+
+    private static final String TRACE_SEPARATOR = "#";
+    private static final IdManager idManager = CloudId.getIdManager();
+
+    /**
+     * Returns the callstack trace contained in request if possible.
+     * @param request request containing header with SPIFFE callstack
+     * @return list calls with first entry being the first call and the last our own id
+     */
+    public static List<String> getFormattedTrace(HttpServletRequest request) {
+        List<String> trace;
+        Enumeration<String> headers = request.getHeaders("X-SPIFFE-Callstack");
+        if (headers.hasMoreElements()) {
+            String raw = headers.nextElement();
+            LOGGER.debug("Received SPIFFE callstack: {}", raw);
+            trace = formatTrace(raw);
+        } else {
+            LOGGER.debug("Received empty SPIFFE callstack");
+            trace = new ArrayList<>();
+            trace.add("No trace available!");
+        }
+        return trace;
+    }
+
+    private static List<String> splitTrace(String trace) {
+        // Remove last TRACE_SEPARATOR which is the last letter of the string
+        trace = trace.trim();
+        trace =trace.substring(0, trace.length() - 1);
+        return Arrays.asList(splitByWholeSeparator(trace, TRACE_SEPARATOR));
+    }
+
+    private static List<String> formatTrace(String raw) {
+        assert raw != null;
+        List<String> trace = new ArrayList<>(splitTrace(raw));
+        trace.add(idManager.getSingleBundle().getSpiffeId());
+        return trace;
+    }
+}
